e6d91629a724dfcabc784681ecdee2a99b120e1b
Guto Maia <guto@guto.net>
2017-11-05 17:06:46 -0200

    now I can use Make patch for new versions

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
+with open(os.path.join(here, 'VERSION.txt')) as f:
+    VERSION = f.read().strip()
+
 with open(os.path.join(here, 'requirements.txt')) as f:
     REQUIREMENTS = [line for line in iter(f) if not line.startswith('--')]
 
 
 setup(
     name='fast_queue',
-    version='0.0.1',
+    version=VERSION,
     description='Simple Task Producer',
     author="Gustavo Maia Neto (Guto Maia)",
     author_email="guto@guto.net",
